0b44d3d1fbfae84ec0353ac74a9e176a095b857d
Chris Guzman <guzmantchris.com>
2013-08-29 18:14:00 -0400

    Added New Relic Gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '56a7111e9426c0fc8765feee0a968ab24e49bafd52699f404cfecad0afb8bbe2adf2d71965ed9eb939fe58e9ca23b7f6a460499b0ec35df1053f4514d001fd4a'
